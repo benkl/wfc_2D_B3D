777e0d5b0aa2cb7e1d2ffbd688d607578af4cf5d
Benjamin Kleinert <benny_kleinert@gmx.de>
2019-10-17 18:39:34 +0200

    implementing in blender, actually working numpy->placement

--- wfc_2d.py
+++ wfc_2d.py
@@ -8,6 +8,8 @@ An example of using the wave function collapse with 2D image.
 import time
 import os
 import numpy as np
+import random
+import sys
 import bpy
 
 # def plot_patterns(patterns, title=''):
@@ -451,10 +453,15 @@ def load_sample(path):
 
     return sample
 
+###########################
+# WELCOME TO BLENDER CITY #
+###########################
 
 # def show(image):
 #     if image.shape[0] == 1:
 #         return plt.imshow(np.squeeze(image, axis=0))
+
+
 class WFC_OT_Runner_2(bpy.types.Operator):
     bl_idname = "object.wfc_ot_runner_2"
     bl_label = "WFC_OT_Runner_2"
@@ -479,6 +486,7 @@ class WFC_OT_Runner_2(bpy.types.Operator):
         #fig, ax = plt.subplots()
         image = wfc.get_image()
         print(image)
+        block_placer(image)
         # im = show(image)
         # while True:
         #     done = wfc.step()
@@ -506,3 +514,80 @@ class WFC_OT_Runner_2(bpy.types.Operator):
 
 # image = wfc.get_image()
 # print(image)
+
+
+def blender_transitions(wfc_result_array):
+    output_tiles = []
+    for x in range(0, len(wfc_result_array)):
+        l_len = len(wfc_result_array[0])
+        for y in range(0, l_len):
+            blender_tile = ['L', 'L', 'L', 'L']
+            if x - 1 >= 0:
+                blender_tile[0] = wfc_result_array[x-1][y]
+            if y + 1 < l_len:
+                blender_tile[1] = wfc_result_array[x][y+1]
+            if x + 1 < len(wfc_result_array):
+                blender_tile[2] = wfc_result_array[x+1][y]
+            if y - 1 >= 0:
+                blender_tile[3] = wfc_result_array[x][y-1]
+            output_tiles.append(blender_tile)
+
+    return output_tiles
+
+
+def get_tiles(tiles):
+    temp_tiles = np.unique(tiles)
+    # l_len = len(tiles[0])
+    # for x in range(0, len(tiles)):
+    #     for y in range(0, l_len):
+    #         if (tiles == temp_tiles).all():
+    #             temp_tiles.append(tiles[x][y])
+    #         # if tiles[x][y] not in temp_tiles:
+    #         #     temp_tiles.append(tiles[x][y])
+    #         else:
+    #             pass
+    return temp_tiles
+
+
+def block_placer(output_array):
+    # Find Collection
+    seed = random.randrange(sys.maxsize)
+    result_name = 'Result ' + str(seed)
+
+    if (bpy.data.collections.find('Tiles') == True):
+        tiles_collection = bpy.data.collections['Tiles']
+    else:
+        tiles_collection = bpy.data.collections.new('Tiles')
+
+    bpy.context.scene.collection.children.link(tiles_collection)
+    l_len = len(output_array[0])
+    variants = get_tiles(output_array)
+    result_collection = bpy.data.collections.new(result_name)
+    bpy.context.scene.collection.children.link(result_collection)
+    for t in range(0, len(variants)):
+        bpy.ops.mesh.primitive_plane_add(location=(t, -5, 0), size=1)
+        basemesh = bpy.context.object
+        basemesh.name = str(variants[t])
+        # bpy.data.collections['Tiles'].objects.link(basemesh)
+        tiles_collection.objects.link(basemesh)
+        # bpy.context.scene.collection.children.unlink(basemesh)
+        print(t)
+
+    for x in range(0, len(output_array)):
+            # in the cell
+        for y in range(0, l_len):
+            print(output_array[x][y][0][0])
+            srcobj = tiles_collection.objects[output_array[x][y]]
+            if srcobj.type == 'MESH':
+                print('mesh')
+                copymesh = srcobj.copy()
+                copymesh.name = output_array[x][y] + str((x + 1) * (y + 1))
+                copymesh.data = srcobj.data
+                # copymesh = bpy.context.object
+                copymesh.location = (x, y, 0)
+                result_collection.objects.link(copymesh)
+            else:
+                pass
+
+        # set mesh name
+    return
