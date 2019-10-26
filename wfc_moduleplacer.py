c0029ffae79b08e0d48604a71c082a3863269575
Benjamin Kleinert <benny_kleinert@gmx.de>
2019-10-26 05:14:51 +0200

    block placement stuff with numpy arrays

--- wfc_moduleplacer.py
+++ wfc_moduleplacer.py
@@ -6,21 +6,6 @@ import bpy
 # PART BELOW NOT WORKING TAKES CHANNEL VALUES INSTEAD OF PIXEL VALUES
 
 """
-def get_tiles(tiles):
-    temp_tiles = np.unique(tiles)
-    # print(temp_tiles)
-    # l_len = len(tiles[0])
-    # for x in range(0, len(tiles)):
-    #     for y in range(0, l_len):
-    #         if (tiles == temp_tiles).all():
-    #             temp_tiles.append(tiles[x][y])
-    #         # if tiles[x][y] not in temp_tiles:
-    #         #     temp_tiles.append(tiles[x][y])
-    #         else:
-    #             pass
-    return temp_tiles
-
-
 def block_placer(output_array):
     # Find Collection
     seed = random.randrange(sys.maxsize)
@@ -63,53 +48,114 @@ def block_placer(output_array):
 
         # set mesh name
     return
-
-
-def blender_transitions(wfc_result_array):
-    output_tiles = []
-    for x in range(0, len(wfc_result_array)):
-        l_len = len(wfc_result_array[0])
-        for y in range(0, l_len):
-            blender_tile = ['L', 'L', 'L', 'L']
-            if x - 1 >= 0:
-                blender_tile[0] = wfc_result_array[x-1][y]
-            if y + 1 < l_len:
-                blender_tile[1] = wfc_result_array[x][y+1]
-            if x + 1 < len(wfc_result_array):
-                blender_tile[2] = wfc_result_array[x+1][y]
-            if y - 1 >= 0:
-                blender_tile[3] = wfc_result_array[x][y-1]
-            output_tiles.append(blender_tile)
-
-    return output_tiles
 """
 
 
-def create_placer_modules(b3d_image):
-    img_source_h = b3d_image.size[0]
-    img_source_w = b3d_image.size[1]
+def blender_image_to_modulelist(b3d_image):
+    img_source_w = b3d_image.size[0]
+    img_source_h = b3d_image.size[1]
+
+    # Pixel target image array
+    pixel_target = np.full((img_source_w, img_source_h), "_9__9__9_")
+    # Get all image pixels
     img_source_array = b3d_image.pixels[:]
-    print(img_source_array)
+
     for height_i in range(0, img_source_h):
+
+        # Make a new line in the row list
 
         for width_i in range(0, img_source_w):
 
             # Get pixel position in flat array
             colar = (width_i + (height_i * img_source_w)) * 4
-            print(colar)
-            # Set color values at current Pixel
 
-            r = round(img_source_array[colar - 4], 8)
-            g = round(img_source_array[colar - 3], 8)
-            b = round(img_source_array[colar - 2], 8)
-            a = round(img_source_array[colar - 1], 8)
-            img_target = []
-            img_target[width_i][height_i][0] = r
-            img_target[width_i][height_i][1] = g
-            img_target[width_i][height_i][2] = b
-            print(img_target[width_i][height_i])
-            # print(r, g, b, a)
-    # print(img_source_array)
+            # Get color values at current "Pixel"
+            r = str(round(img_source_array[colar - 4], 1))
+            g = str(round(img_source_array[colar - 3], 1))
+            b = str(round(img_source_array[colar - 2], 1))
+            rgb = r+g+b
+
+            # Append a list of the rgb values for each pixel to the line list
+            pixel_target[height_i][width_i] = rgb
+
+    # Empty target module array
+    module_target = np.full((img_source_w-2, img_source_h-2),
+                            "45_45_45_45_45_45_45_45_45_45_45_45_45_45_45_")
+    for height_i in range(0, img_source_h-2):
+
+        # Append new line to module row
+        for width_i in range(0, img_source_w-2):
+
+            # Return a list of neighbours
+            current_module = pixelstr_neighbours_list(
+                height_i, width_i, pixel_target)
+            module_target[height_i][width_i] = current_module
+
+    return module_target
+
+
+def pixelstr_neighbours_list(x_coord, y_coord, pixel_list):
+
+    # Append neighbours
+    zero = pixel_list[x_coord][y_coord]
+    xminus = pixel_list[x_coord-1][y_coord]
+    yplus = pixel_list[x_coord][y_coord+1]
+    xplus = pixel_list[x_coord + 1][y_coord]
+    yminus = pixel_list[x_coord][y_coord-1]
+    neighbours_list = zero + xminus + yplus + xplus + yminus
+    return neighbours_list
+
+
+def place_blocks(module_list):
+
+    # Find Collection
+    seed = random.randrange(sys.maxsize)
+    result_name = 'Result ' + str(seed)
+    unique_tiles = np.unique(module_list)
+
+    if (bpy.data.collections.find('Tiles') == True):
+        tiles_collection = bpy.data.collections['Tiles']
+    else:
+        tiles_collection = bpy.data.collections.new('Tiles')
+
+    bpy.context.scene.collection.children.link(tiles_collection)
+    print(unique_tiles)
+
+    for t in range(0, len(unique_tiles)):
+
+        bpy.ops.mesh.primitive_plane_add(location=(t, -5, 0), size=1)
+
+        basemesh = bpy.context.object
+
+        basemesh.name = unique_tiles[t]
+
+        tiles_collection.objects.link(basemesh)
+
+    # result_collection = bpy.data.collections.new(result_name)
+
+    # bpy.context.scene.collection.children.link(result_collection)
+
+    # for x in range(0, len(module_list)):
+
+    #     for y in range(0, len(module_list[x])):
+
+    #         srcobj = tiles_collection.objects[module_list[x][y]]
+
+    #         if srcobj.type == 'MESH':
+
+    #             print('mesh')
+    #             copymesh = srcobj.copy()
+    #             copymesh.name = module_list[x][y] + str((x + 1) * (y + 1))
+    #             copymesh.data = srcobj.data
+
+    #             # copymesh = bpy.context.object
+    #             copymesh.location = (x, y, 0)
+    #             result_collection.objects.link(copymesh)
+    #         else:
+    #             pass
+
+    # set mesh name
+
     return
 
 
@@ -120,5 +166,6 @@ class WFC_OT_Placer(bpy.types.Operator):
     def execute(self, context):
         img_name = bpy.context.scene.wfc_vars.wfc_images_placer
         img = bpy.data.images[img_name]
-        create_placer_modules(img)
+        module_list = blender_image_to_modulelist(img)
+        place_blocks(module_list)
         return {'FINISHED'}
